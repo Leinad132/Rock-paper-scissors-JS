7732a503573053963e21a6f766ffc596a2640918
Daniel Murati <murati_d@yahoo.com>
2024-01-22 23:04:22 -0500

    finalized the program by adding the loop that plays 5 rounds and shows the score

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 let choices = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
  
 function computerChoice(choices){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-let computer = computerChoice(choices);
+//let computer = computerChoice(choices);
 
-let input = prompt("Enter your answer:");
+//let input = prompt("Enter your answer:");
 
 function playerChoice(input){
     return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
 }
 
-let player = playerChoice(input);
+//let player = playerChoice(input);
 
 function playRound(player,computer){
     switch(player){
@@ -21,9 +24,13 @@ function playRound(player,computer){
                 case "Rock":
                     return("Its a tie");
                 case "Paper":
+                    computerScore++;
                     return("Computer Wins, paper beats rock!");
+                    
                 case "Scissors":
+                    playerScore++;
                     return("you win. Rock breaks scissors");
+                    
             }
             break;
     }
@@ -33,24 +40,30 @@ function playRound(player,computer){
                 case "Paper":
                     return("its a tie.");
                 case "Rock":
+                    playerScore++;
                     return("player wins, paper beats rock");
+                    
                 case "Scissors":
-                    return("player loses.Scissors beat paper")
+                    computerScore++;
+                    return("player loses.Scissors beat paper");
+                    
             }
             break;
     }
-    switch([player]){
+    switch(player){
         case"Scissors":
             switch(computer){
                 case"Scissors":
                     return("its a tie.");
                 
                 case"Paper":
+                    playerScore++;
                     return("player wins, scissors cut paper");
-                
+                    
                 case"Rock":
+                    playerScore++;
                     return("computer wins,rock beats scissors");
-            }
+            }       
             break;
     
 
@@ -58,7 +71,41 @@ function playRound(player,computer){
     
 }
 
-console.log(playRound(player,computer));
+function game(){
+    for(round = 1; round <= 5; round++){
+        let computer = computerChoice(choices);
 
-console.log("player entered:" + player);
-console.log("computer entered:" + computer);
+        let input = prompt("Enter your answer:");
+        let player = playerChoice(input);
+        console.log("Player entered: "+ player);
+        console.log("Computer entered: "+ computer);
+
+        let result = playRound(player,computer);
+        console.log(result);
+
+        console.log("\nPlayer score: "+ playerScore);
+        console.log("Computer score: "+ computerScore);
+
+        /*if(playerScore < computerScore){
+            console.log("Computer wins the game!");
+        } else{
+            console.log("Player wins the game!");
+        }*/
+
+    }
+    if(playerScore < computerScore){
+        console.log("Computer wins the game!");
+    } else if(playerScore > computerScore){
+        console.log("Player wins the game!");
+    }else {
+        console.log("Game ends in a tie");
+    }
+}
+
+game();
+
+
+//console.log(playRound(player,computer));
+
+//console.log("player entered:" + player);
+//console.log("computer entered:" + computer);
